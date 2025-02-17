c30a6865a690ede27d2d2075a385ff7e6696663d
Allen <star359025765@gmail.com>
2025-02-17 17:44:47 +0800

    feat(defi): borrow DAI from AAVE using WETH.

--- hardhat-defi/scripts/aaveBorrow.js
+++ hardhat-defi/scripts/aaveBorrow.js
@@ -4,6 +4,9 @@ const { getNetworkConfig } = require("../helper-hardhat-config")
 
 const conf = getNetworkConfig()
 
+const RATE_MODE = 2
+const BORROW_RATE = 0.8
+
 async function main() {
     await getWeth()
     const { deployer } = await getNamedAccounts()
@@ -20,9 +23,23 @@ async function main() {
 
     console.log("starting to get borrower data...")
     let { availableBorrowsETH, totalDebtETH } = await getBorrowerData(lendingPool, deployer)
+    availableBorrowsETH = Number(availableBorrowsETH)
+    totalDebtETH = Number(totalDebtETH)
+    console.log(`You have ${totalDebtETH / 1e18} worth of ETH borrowed.`)
+    console.log(`You can borrow ${availableBorrowsETH / 1e18} worth of ETH.`)
 
     console.log("starting to get DAI/ETH price...")
-    const price = await getDAIETHPrice()
+    const daiEthPrice = await getDAIETHPrice()
+
+    const amountDaiToBorrow = availableBorrowsETH * BORROW_RATE * (1 / Number(daiEthPrice))
+    console.log(`you can borrow ${amountDaiToBorrow} DAI...`)
+
+    const borrowEth = ethers.parseEther(amountDaiToBorrow.toString())
+
+    await borrowDai(lendingPool, borrowEth, deployer)
+
+    await approveErc20(conf.daiToken, lendingPool.target, borrowEth, signer)
+    await repayDai(lendingPool, borrowEth, deployer)
 }
 
 async function getLendingPool(signer) {
@@ -44,9 +61,7 @@ async function approveErc20(erc20Address, spenderAddress, amount, signer) {
 
 async function getBorrowerData(lendingPool, account) {
     const { totalCollateralETH, totalDebtETH, availableBorrowsETH } = await lendingPool.getUserAccountData(account)
-    console.log(`You have ${ethers.parseEther(totalCollateralETH)} worth of ETH deposited.`)
-    console.log(`You have ${totalDebtETH} worth of ETH borrowed.`)
-    console.log(`You can borrow ${ethers.parseEther(availableBorrowsETH)} worth of ETH.`)
+    console.log(`You have ${Number(totalCollateralETH) / 1e18} worth of ETH deposited.`)
     return { availableBorrowsETH, totalDebtETH }
 }
 
@@ -57,6 +72,22 @@ async function getDAIETHPrice() {
     const decimals = await priceFeed.decimals()
     console.log(`DAI/ETH price: ${(Number(price) / 10 ** Number(decimals)).toFixed(8)}`)
     return price
+}
+
+async function borrowDai(lendingPool, amount, account) {
+    const tx = await lendingPool.borrow(conf.daiToken, amount, RATE_MODE, 0, account)
+    tx.wait(1)
+    console.log("Borrowed!")
+    const { totalDebtETH } = await getBorrowerData(lendingPool, account)
+    console.log(`Now you have already borrowed ${Number(totalDebtETH) / 1e18} worth of ETH to exchange DAI.`)
+}
+
+async function repayDai(lendingPool, amount, account) {
+    const tx = await lendingPool.repay(conf.daiToken, amount, RATE_MODE, account)
+    tx.wait(1)
+    console.log("Repaid!")
+    const { availableBorrowsETH } = await getBorrowerData(lendingPool, account)
+    console.log(`You can borrow ${Number(availableBorrowsETH) / 1e18} worth of ETH.`)
 }
 
 main()
